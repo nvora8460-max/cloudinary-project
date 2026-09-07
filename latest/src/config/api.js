325cba5fe935f9dec7d4c461ef76bc0e4630a6ee
Nishtha vora <nvora8460@gmail.com>
2026-09-07 11:40:55 +0530

    fix: api.js

--- latest/src/config/api.js
+++ latest/src/config/api.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/images`,
-    headers: {
-        "Content-Type": "application/json",
-    },
 });
 
 export default api;
